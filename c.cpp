0534f78622738e153d11dba9d767de7b9e694b96
Pushkraj001 <pushkrajshahane1234@gmail.com>
2024-12-04 20:37:58 +0530

    .

---
+++ c.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Include the necessary header for input/output
+using namespace std;
+
+int main() {
+    cout << "Hello";  // Correct the output stream syntax
+    return 0;  // Return 0 to indicate successful program execution
+}
